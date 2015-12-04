ce8f7289d941717a4df04e4431da36a61cc9c3b4
David Webb <David_Webb@jabil.com>
2015-12-04 09:39:29 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Scratch_3"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.1"
---
+++ src/main/scala-2.11/Scratch_3/Scratch_3.scala
@@ -0,0 +1,11 @@
+package Scratch_3
+
+/**
+  * Created by WebbD on 12/4/2015.
+  */
+object Scratch_3 {
+  def main(args: Array[String]){
+    println("Finished.")
+  }
+
+}
